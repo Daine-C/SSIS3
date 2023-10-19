c9f4fc32a1a488c461e62ff637f49a05c26dac20
Daine-C <danielleverna.caasi@g.msuiit.edu.ph>
2023-10-20 05:34:05 +0800

    Database Complete

--- app.py
+++ app.py
@@ -26,6 +26,20 @@ class Courses(db.Model):
     def __repr__(self):
         return '<Course %r>' % self.id
     
+class Students(db.Model):
+    __tablename__ = "Students"
+    id = db.Column(db.String(10), primary_key = True, nullable = False)
+    firstname = db.Column(db.String(200), nullable = False)
+    lastname = db.Column(db.String(200), nullable = False)
+    year = db.Column(db.Integer, nullable = False)
+    gender = db.Column(db.String(200), nullable = False)
+    #Course reference
+    courseid = db.Column(db.String(10), db.ForeignKey(Courses.id))
+    c_college = db.relationship(Courses, cascade = "all,delete", backref = "students")
+
+    def __repr__(self):
+        return '<Student %r>' % self.id
+    
 with app.app_context():
     db.create_all()
 
@@ -78,26 +92,38 @@ def course():
 
         return render_template('Courses.html', cours=cours)
 
-@app.route('/colleges/', methods=['POST', 'GET'])    
+@app.route('/students/', methods=['POST', 'GET'])    
 def student():
     if request.method == 'POST':
-        college_id = request.form['id']
-        college_name = request.form['name']
-        new_college = Colleges(id=college_id)
-        name_college = Colleges(name=college_name)
+        student_id = request.form['id']
+        student_fname = request.form['firstname']
+        student_lname = request.form['lastname']
+        student_year = request.form['year']
+        student_gender = request.form['gender']
+        student_course = request.form['course']
+        new_student = Students(id=student_id)
+        fname_student = Students(firstname=student_fname)
+        lname_student = Students(secondname=student_lname)
+        year_student = Students(year=student_year)
+        gender_student = Students(id=student_gender)
+        conn_student = Students(name=student_course)
 
         try:
-            db.session.add(new_college)
-            db.session.add(name_college)
+            db.session.add(new_student)
+            db.session.add(fname_student)
+            db.session.add(lname_student)
+            db.session.add(year_student)
+            db.session.add(gender_student)
+            db.session.add(conn_student)
             db.session.commit()
             return redirect('/')
         except:
             return 'Error :('
         
     else:
-        colgs = Colleges.query.order_by(Colleges.id).all()
+        stud = Students.query.order_by(Students.id).all()
 
-        return render_template('College.html', colgs=colgs)        
+        return render_template('Students.html', stud=stud)        
 
 if __name__ == "__main__":
     app.run(debug=True)
